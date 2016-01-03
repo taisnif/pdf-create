9404b86a0dc738b2998fa48f35071c56838a5147
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-01-03 00:42:03 +0000

    - Merged GitHub Pull Request #9 from ESERTE.

--- lib/PDF/Create.pm
+++ lib/PDF/Create.pm
@@ -1,6 +1,6 @@
 package PDF::Create;
 
-our $VERSION = '1.25';
+our $VERSION = '1.26';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ PDF::Create - Create PDF files.
 
 =head1 VERSION
 
-Version 1.25
+Version 1.26
 
 =cut
 
